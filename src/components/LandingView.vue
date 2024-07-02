073a3f2c293a8984da046346bd00c161306bc403
asheekasamuels <samuelsasheeka133@gmail.com>
2024-07-02 08:19:11 +0200

    landing

--- src/components/LandingView.vue
+++ src/components/LandingView.vue
@@ -1,5 +1,24 @@
 <template>
-    <div class="landing">
-        <h1>This is my landing page</h1>
+    <div class="home" :style="image">
+      <div class="landing">
+        <div class="landing-view">
+            <h1>Asheeka Samuels Portfolio</h1>
+                <p>{{ jobTitle }}</p>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
+  
+  <script>
+  export default {
+  name: 'LandingView',
+  computed: {
+    jobTitle() {
+      return this.$store.state.jobTitle; // Assuming jobTitle is an array and we want the first item
+    }
+  },
+  mounted() {
+    this.$store.dispatch('fetchjobTitle'); // Assuming 'fetchjobTitle' is the action to fetch job title data
+  }
+};
+  </script>
