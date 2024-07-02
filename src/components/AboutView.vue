df1c60d6b1cda83651fc22ba092ad11557ff28a4
asheekasamuels <samuelsasheeka133@gmail.com>
2024-07-02 09:08:45 +0200

    about

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -1,32 +1,25 @@
 <template>
-  <div class="container p-3" id="about">
-  <h1 id="heading">About Me</h1>
-  <div class="infoBlocks container-fluid">
-    <div class="leftSide">
-      <div class="text-col" v-if="about">
-        <p class="lead" v-for="about in about" :key="about">{{ about }}</p>
+  <div class="container about-container">
+    <h1>About</h1>
+    <div class="row">
+      <div class="col">
+        <img src="https://asheekasamuels.github.io/All-Images/images/Asheeka3.jpg" alt="keira" class="img-fluid w-75" loading="lazy">
       </div>
-    </div>
-    <div class="rightSide">
-      <div class="pic-col">
-        <img src="https://asheekasamuels.github.io/All-Images/images/Asheeka3.jpg" alt="Asheeka3" class="pic img-fluid">
+      <div class="col d-flex flex-column justify-content-center align-items-center">
+        <p v-for="(item, index) in about" :key="index">{{ item }}</p>
+        <Spinner v-if="!about.length"/>
       </div>
     </div>
   </div>
-</div>
-
 </template>
 
-<script>
-export default {
-  name: 'AboutView',
-  computed: {
-    about(){
-      return this.$store.state.about
-    }
-  },
-  mounted(){
-    this.$store.dispatch('fetchAbout')
-  }
-};
+<script setup>
+import Spinner from './Spinner.vue'
+import { computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+const about = computed(() => store.state.about)
+onMounted(() => {
+  store.dispatch('fetchAbout')
+})
 </script>
